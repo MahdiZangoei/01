07bd2faf916fbb63781ad8f83d028ec114683cdc
MahdiZangoei <mahdizangoei1@gmail.com>
2023-05-26 21:39:09 +0200

    Initial commit

---
+++ hi-to-me/hi-to-me/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hi_to_me
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name: ");
+            string User_Name = Console.ReadLine();
+            Console.WriteLine("Hello "+ User_Name + "\n! Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ number - 3/number - 3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace number___3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number : ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("The tripled number is : "+ number * 3);
+        }
+    }
+}
